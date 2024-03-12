cdc6d992a064af58589a25281336ca530aa60dd3
Nicolas Ala <nicoalmeda96@gmail.com>
2024-03-12 03:40:13 -0300

    correcion para que renderice aunque no tenga json server funcionando

--- src/components/AdminRecetas.jsx
+++ src/components/AdminRecetas.jsx
@@ -25,28 +25,40 @@ const AdminRecetas = () => {
 
 
   return (
-    <Container fluid className="mainContainer">
-      <div className="d-flex justify-content-between align-items-center mt-5">
-        <h1 className="display-4 ">Productos disponibles</h1>
-      </div>
-      <div className="table-responsive">
-        <Table responsive striped bordered hover>
-          <thead>
-            <tr className="text-center">
-              <th>Cod</th>
-              <th>Receta</th>
-              <th>URL de Imagen</th>
-              <th>Descripcion</th>
-            </tr>
-          </thead>
-          <tbody>
-            {recetas.map((receta) => (
-              <ItemRecetas key={receta.id} receta={receta} setRecetas={setRecetas} />
-            ))}
-          </tbody>
-        </Table>
-      </div>
-    </Container>
+    
+
+    
+      <Container fluid className="mainContainer">
+        {recetas && recetas.length > 0 ? (
+          <>
+          <div className="d-flex justify-content-between align-items-center mt-5">
+            <h1 className="display-4 ">Productos disponibles</h1>
+          </div>
+        <div className="table-responsive">
+          <Table responsive striped bordered hover>
+            <thead>
+              <tr className="text-center">
+                <th>Cod</th>
+                <th>Receta</th>
+                <th>URL de Imagen</th>
+                <th>Descripcion</th>
+              </tr>
+            </thead>
+            <tbody>
+              {recetas.map((receta) => (
+                <ItemRecetas key={receta.id} receta={receta} setRecetas={setRecetas} />
+              ))}
+            </tbody>
+          </Table>
+        </div>
+        </>
+          ):(
+          <div className="d-flex justify-content-center mt-5">
+          <h1 className="display-4 ">No hay recetas disponibles</h1>
+        </div>
+          )}
+        
+      </Container>
   );
 };
 
